12dedf4dcac8522612b56902788b9ea71d2b1bc1
Priyansh <Priyanshyadav1012@gmail.com>
2024-12-19 02:45:25 +0530

    Implement the React-Router-Dom and Create Custom Color via TailwindCss

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,32 @@
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Doctor from "./pages/Doctor";
+import Login from "./pages/Login";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import MyProfile from "./pages/MyProfile";
+import MyAppointment from "./pages/MyAppointment";
+import Appointment from "./Appointment";
+import NavBar from "./components/NavBar";
+
 const App = () => {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <div className="mx-4 sm:mx-[10%]">
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/doctors" element={<Doctor />} />
+        <Route path="/doctors/: speciality" element={<Doctor />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/my-profile" element={<MyProfile />} />
+        <Route path="/my-appointment" element={<MyAppointment />} />
+        <Route path="/appointment/:docId" element={<Appointment />} />
+      </Routes>
+    </div>
+  );
 };
 
 export default App;
---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,9 @@
+const Home = () => {
+  return (
+    <div>
+      <p className="text-black font-mono text-3xl">HomePage</p>
+    </div>
+  );
+};
+
+export default Home;

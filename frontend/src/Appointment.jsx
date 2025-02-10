0efd4515d69d5716e477ef5eb2141bcc67e7f2b1
Priyansh <Priyanshyadav1012@gmail.com>
2025-02-10 12:54:34 +0530

    Appointment page is in Progress

--- frontend/src/Appointment.jsx
+++ frontend/src/Appointment.jsx
@@ -17,7 +17,52 @@ const Appointment = () => {
     setDocInfo(docInfo);
   };
 
-  const getAvailableSlots = async () => {};
+  const getAvailableSlots = async () => {
+    setDocSlots([]);
+
+    //getting current date
+    let today = new Date();
+
+    for (let i = 0; i < 7; i++) {
+      //getting days with index
+      let currentDate = new Date(today);
+      currentDate.setDate(today.getDate() + i);
+
+      //setting endTime of the date with index
+      let endTime = new Date();
+      endTime.setDate(today.getDate() + 1);
+      endTime.setHours(21, 0, 0, 0);
+
+      //setting hours
+      if (today.getDate() === currentDate.getDate()) {
+        currentDate.setHours(
+          currentDate.getHours() > 10 ? currentDate.getHours() + 1 : 10
+        );
+        currentDate.setMinutes(currentDate.getMinutes() > 30 ? 30 : 0);
+      } else {
+        currentDate.setHours(10);
+        currentDate.setMinutes(0);
+      }
+
+      let timeSlots = [];
+
+      while (currentDate < endTime) {
+        let formattedTime = currentDate.toLocaleDateString([], {
+          hour: "2-digits",
+          minute: "2-digit",
+        });
+
+        //add slot to array
+        timeSlots.push({
+          dateTime: new Date(currentDate),
+          time: formattedTime,
+        });
+
+        //INcrement Current Time by 30 minutes
+        currentDate.setMinutes(currentDate.getMinutes() + 30);
+      }
+    }
+  };
 
   useEffect(() => {
     getAvailableSlots();

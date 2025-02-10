07f4800109024e5ab7f1e8bf0ea4636555a525a5
Priyansh <Priyanshyadav1012@gmail.com>
2025-02-10 11:19:35 +0530

    Appointment page is in Progress

--- frontend/src/Appointment.jsx
+++ frontend/src/Appointment.jsx
@@ -1,17 +1,83 @@
-import { useContext, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { AppContext } from "./context/AppContext";
+import { assets } from "./assets/assets";
 
 const Appointment = () => {
   const { docId } = useParams();
-  const { doctors } = useContext(AppContext);
+  const { doctors, currencySymbol } = useContext(AppContext);
+
   const [docInfo, setDocInfo] = useState(null);
+  const [docSlot, setDocSlots] = useState([]);
+  const [slotIndex, setSlotIndex] = useState(0);
+  const [slotTime, setSlotTime] = useState("");
 
   const fetchDocInfo = async () => {
     const docInfo = doctors.find((doc) => doc._id === docId);
     setDocInfo(docInfo);
   };
-  return <div>Appointment</div>;
+
+  const getAvailableSlots = async () => {};
+
+  useEffect(() => {
+    getAvailableSlots();
+  }, [docInfo]);
+
+  useEffect(() => {
+    fetchDocInfo();
+  }, [doctors, docId]);
+
+  return (
+    docInfo && (
+      <div>
+        {/* Doctor Details */}
+        <div className="flex flex-col sm:flex-row gap-4">
+          <div>
+            <img
+              src={docInfo.image}
+              alt="Doctor Image"
+              className="bg-primary w-full sm:max-w-72 rounded-lg "
+            />
+          </div>
+
+          <div className="flex-1 border border-gray-400 py-8 rounded-lg p-8 bg-white mx-2 sm:mx-0 mt-[-80px] sm:mt-0">
+            {/* Doctor Info Like Name degree And Expericence */}
+            <p className="flex items-center gap-2 text-2xl font-medium text-gray-900">
+              {docInfo.name}{" "}
+              <img
+                src={assets.verified_icon}
+                alt="verified_icons"
+                className="w-5"
+              />
+            </p>
+            <div className="flex items-center gap-2 text-sm mt-1 text-gray-600">
+              {docInfo.degree} - {docInfo.speciality}
+              <button className="py-0.5 px-2 border text-sm rounded-full">
+                {docInfo.experience}
+              </button>
+            </div>
+
+            {/* Doctor About */}
+            <div>
+              <p className="flex items-center gap-1 text-sm font-medium text-gray-900 mt-3">
+                About <img src={assets.info_icon} alt="Info icon" />
+              </p>
+              <p className="text-sm text-gray-500 max-w-[-70p0x] mt-1">
+                {docInfo.about}
+              </p>
+            </div>
+            <p className="text-gray-500 font-medium mt-4">
+              Appointment Fee :
+              <span className="text-gray-900">
+                {currencySymbol}
+                {docInfo.fees}
+              </span>
+            </p>
+          </div>
+        </div>
+      </div>
+    )
+  );
 };
 
 export default Appointment;
--- frontend/src/context/AppContext.jsx
+++ frontend/src/context/AppContext.jsx
@@ -7,8 +7,11 @@ import { doctors } from "../assets/assets";
 export const AppContext = createContext();
 
 const AppContextProvider = ({ children }) => {
+  const currencySymbol = "$";
+
   const value = {
     doctors,
+    currencySymbol,
   };
 
   return <AppContext.Provider value={value}>{children}</AppContext.Provider>;
